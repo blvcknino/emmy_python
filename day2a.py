56d88c66cf10ec64b0931d629812c0c9f3e3cacd
blvcknino <94177821+blvcknino@users.noreply.github.com>
2021-11-12 14:17:09 +0100

    Add files via upload

---
+++ day2a.py
@@ -0,0 +1,30 @@
+ade = 30000
+nkechi = 50000
+bola = 150000
+total = sum([ade,nkechi,bola])
+summary ='at the end of todays activities the total deposit that came in is {},ade deposited the sum of {},nkechi deposited {},bola deposited {}'
+print(summary.format(total,ade,nkechi,bola))
+print(f'at the end of todays activities the total deposit that came in is {total} ,ade deposited the sum of {ade},nkechi deposited {nkechi},bola deposited {bola} ')
+
+cedarcollege = 'school'
+federal = 'SCHOOL'
+chineme = 'python is an high level programming language'
+great = '   emmanuelnwadike92@gmail.com'
+print(cedarcollege.upper())
+print(federal.lower())
+print(chineme.capitalize())
+print(chineme.title())
+print(chineme.split())
+print(great)
+print(great.strip())
+
+number1 = 120
+number2 = 120.5
+number3 = 'python'
+number4 = '50'
+#an integer can be converted to string and float
+#you can convert float to integer and string
+#we cannot convert a string to integer or float
+#but we can convert a number in a string to integer or float
+print(str(number1))
+print(int(number4))
